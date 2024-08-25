14f7e672c4c1631e713b66d4a3c4246fa69279e9
Dinadedt <Dina Gonzalez>
2024-08-25 12:25:17 -0600

    card 2 y 3

--- dina-patrocinadores.js
+++ dina-patrocinadores.js
@@ -5,8 +5,8 @@ document.querySelectorAll('.info-button').forEach((button, index) => {
         // Array con los enlaces de los patrocinadores
         let enlaces = [
             "https://www.facebook.com/photo/?fbid=583193780282437&set=a.175993924335760",
-            "https://www.enlace2.com",
-            "https://www.enlace3.com"
+            "https://www.facebook.com/photo/?fbid=185463733822127&set=a.185463740488793",
+            "https://www.iba.gob.sv/"
         ];
         // Redirige al enlace correspondiente basado en el índice del botón clickeado
         window.location.href = enlaces[index];
