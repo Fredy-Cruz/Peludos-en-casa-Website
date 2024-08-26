d216ee7f62006e1d3a1c210b5c7bd32e5d5bb6ca
AlejandroAHF <aleherfernandez@gmail.com>
2024-08-25 21:41:05 -0600

    Creation of labels through dom manipulation

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const footer = document.querySelector("footer");
+
+footer.insertAdjacentHTML('beforeend',`
+    <div class="header-footer">
+        <img src="https://res.cloudinary.com/dksv7n9bg/image/upload/v1724626878/ny23uybcovzdog6zpoyr.svg" alt="">
+    </div>
+    <div class="information-footer">
+        <h3>Información de Contacto</h3>
+        <a href="#">Teléfono(s) de contacto</a>
+        <a href="#">Correo electrónico</a>
+        <a href="#">Dirección física (si hay oficinas o refugios)</a>
+    </div>
+    <div class="ImportantLinks-footer">
+        <h3>Enlaces Importantes</h3>
+        <a href="#">Políticas de privacidad</a>
+        <a href="#">Términos y condiciones</a>
+        <a href="#">Preguntas frecuentes (FAQ)</a>
+        <a href="#">Enlace a la página de donaciones</a>
+    </div>
+    <div class="MainSections-footer">
+        <h3>Enlace a las Secciones Principales</h3>
+        <a href="#">Enlace a la página de adopción</a>
+        <a href="#">Enlace a la página de voluntariado</a>
+        <a href="#">Enlace al blog o noticias</a>
+    </div>
+    <div class="SocialNetworks-footer">
+        <h3>Redes Sociales</h3>
+        <i class="fa-brands fa-x-twitter"></i>
+        <i class="fa-brands fa-facebook"></i>
+        <i class="fa-brands fa-whatsapp"></i>
+        <i class="fa-brands fa-instagram"></i>
+    </div>
+`);
